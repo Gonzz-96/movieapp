33f24ebb2adfb53339bfa70420ba6c509bf2f065
Gonzalo Campos <gonzalocampose1@gmail.com>
2025-12-26 19:23:38 -0600

    Add grpc to metadata service.

--- metadata/cmd/main.go
+++ metadata/cmd/main.go
@@ -1,52 +1,30 @@
 package main
 
 import (
-	"context"
-	"flag"
-	"fmt"
 	"log"
-	"net/http"
-	"time"
+	"net"
 
+	"google.golang.org/grpc"
+	"movieexample.com/gen"
 	metadata "movieexample.com/metadata/internal/controller"
-	httphandler "movieexample.com/metadata/internal/handler/http"
+	grpchandler "movieexample.com/metadata/internal/handler/grpc"
 	"movieexample.com/metadata/internal/repository/memory"
-	"movieexample.com/pkg/discovery/consul"
-	discovery "movieexample.com/pkg/registry"
 )
 
 const serviceName = "metadata"
 
 func main() {
-	var port int
-	flag.IntVar(&port, "port", 8081, "API handler port")
-	flag.Parse()
-	log.Printf("Starting the metadata service on port %d", port)
-	registry, err := consul.NewRegistry("localhost:8500")
-	if err != nil {
-		panic(err)
-	}
-	ctx := context.Background()
-	instanceID := discovery.GenerateInstanceID(serviceName)
-	if err := registry.Register(ctx, instanceID, serviceName, fmt.Sprintf("localhost:%d", port)); err != nil {
-		panic(err)
-	}
-	go func() {
-		for {
-			if err := registry.ReportHealthyState(instanceID, serviceName); err != nil {
-				log.Println("Failed to report healthy state: " + err.Error())
-			}
-			time.Sleep(1 * time.Second)
-		}
-	}()
-
-	defer registry.Deregister(ctx, instanceID, serviceName)
-
+	log.Println("Starting the movie metadata service.")
 	repo := memory.New()
-	ctrl := metadata.New(repo)
-	h := httphandler.New(ctrl)
-	http.Handle("/metadata", http.HandlerFunc(h.GetMetadata))
-	if err := http.ListenAndServe(":8081", nil); err != nil {
+	svc := metadata.New(repo)
+	h := grpchandler.New(svc)
+	lis, err := net.Listen("tcp", "localhost:8081")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	srv := grpc.NewServer()
+	gen.RegisterMetadataServiceServer(srv, h)
+	if err := srv.Serve(lis); err != nil {
 		panic(err)
 	}
 }
