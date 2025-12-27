52a69abc17b8dfc1beddb2f054bf559e842a9501
Gonzalo Campos <gonzalocampose1@gmail.com>
2025-12-26 19:30:56 -0600

    Add grpc to rating service.

--- rating/cmd/main.go
+++ rating/cmd/main.go
@@ -1,52 +1,30 @@
 package main
 
 import (
-	"context"
-	"flag"
-	"fmt"
 	"log"
-	"net/http"
-	"time"
+	"net"
 
-	"movieexample.com/pkg/discovery/consul"
-	discovery "movieexample.com/pkg/registry"
+	"google.golang.org/grpc"
+	"movieexample.com/gen"
 	rating "movieexample.com/rating/internal/controller"
-	httphandler "movieexample.com/rating/internal/handler/http"
+	grpchandler "movieexample.com/rating/internal/handler/grpc"
 	"movieexample.com/rating/internal/repository/memory"
 )
 
 const serviceName = "rating"
 
 func main() {
-	var port int
-	flag.IntVar(&port, "port", 8082, "API handler port")
-	flag.Parse()
-	log.Printf("Starting the rating service on port %d", port)
-	registry, err := consul.NewRegistry("localhost:8500")
-	if err != nil {
-		panic(err)
-	}
-	ctx := context.Background()
-	instanceID := discovery.GenerateInstanceID(serviceName)
-	if err := registry.Register(ctx, instanceID, serviceName, fmt.Sprintf("localhost:%d", port)); err != nil {
-		panic(err)
-	}
-	go func() {
-		for {
-			if err := registry.ReportHealthyState(instanceID, serviceName); err != nil {
-				log.Println("Failed to report healthy state: " + err.Error())
-			}
-			time.Sleep(1 * time.Second)
-		}
-	}()
-	defer registry.Deregister(ctx, instanceID, serviceName)
-
 	log.Println("Starting the rating service")
 	repo := memory.New()
-	ctrl := rating.New(repo)
-	h := httphandler.New(ctrl)
-	http.Handle("/rating", http.HandlerFunc(h.Handle))
-	if err := http.ListenAndServe(":8082", nil); err != nil {
+	svc := rating.New(repo)
+	h := grpchandler.New(svc)
+	lis, err := net.Listen("tcp", "localhost:8082")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	srv := grpc.NewServer()
+	gen.RegisterRatingServiceServer(srv, h)
+	if err := srv.Serve(lis); err != nil {
 		panic(err)
 	}
 }
