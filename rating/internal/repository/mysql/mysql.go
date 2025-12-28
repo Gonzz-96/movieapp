42233a2921c8b2fd3afe8cf2476ee2f87ab895c9
Gonzalo Campos <gonzalocampose1@gmail.com>
2025-12-27 19:22:04 -0600

    Implement mysql driver in rating repo.

---
+++ rating/internal/repository/mysql/mysql.go
@@ -0,0 +1,56 @@
+package mysql
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+
+	_ "github.com/go-sql-driver/mysql"
+	"movieexample.com/rating/pkg/model"
+)
+
+// Repository defines a MySQL-based rating repository.
+type Repository struct {
+	db *sql.DB
+}
+
+// New creates a new MySQL-based rating repository.
+func New() (*Repository, error) {
+	db, err := sql.Open("mysql", "root:password@/movieexample")
+	if err != nil {
+		return nil, err
+	}
+	return &Repository{db}, nil
+}
+
+func (r *Repository) Get(ctx context.Context, recordID model.RecordID, recordType model.RecordType) ([]model.Rating, error) {
+	rows, err := r.db.QueryContext(ctx, "SELECT user_id, value FROM ratings WHERE record_id = ? AND record_type = ?", recordID, recordType)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	var res []model.Rating
+	for rows.Next() {
+		var userID string
+		var value int32
+		if err := rows.Scan(&userID, &value); err != nil {
+			return nil, err
+		}
+		res = append(res, model.Rating{
+			UserID:     model.UserID(userID),
+			Value:      model.RatingValue(value),
+			RecordType: string(recordType),
+			RecordID:   string(recordID),
+		})
+	}
+	return res, nil
+}
+
+func (r *Repository) Put(ctx context.Context, recordID model.RecordID, recordType model.RecordType, rating *model.Rating) error {
+	if rating == nil {
+		return errors.New("rating is nil")
+	}
+	_, err := r.db.ExecContext(ctx, "INSERT INTO ratings (record_id, record_type, user_id, value) VALUES (?, ?, ?, ?)",
+		recordID, recordType, rating.UserID, rating.Value)
+	return err
+}
